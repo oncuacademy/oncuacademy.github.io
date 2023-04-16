124654da67eecb4af511264d225b32ffbf83e070
Tarik Kavaz <tarikkavaz@gmail.com>
2023-04-16 22:23:35 +0300

    generate again içerik

--- _nuxt/static/1681667006/tr/certificate-programs-and-courses/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__('/tr/certificate-programs-and-courses', { data: [{}], fetch: {}, mutations: [] })
---
+++ _nuxt/static/1681672999/en/chairs-message/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/en/chairs-message", {data:[{}],fetch:{},mutations:[]});
